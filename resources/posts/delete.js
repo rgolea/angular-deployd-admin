f1aeae293709998d86d41dbaa8b07e4ea1d1cbfb
Rares G <rgolea@gmail.com>
2015-05-21 21:45:01 +0200

    Changed author display on the post preview

--- resources/posts/delete.js
+++ resources/posts/delete.js
@@ -1,12 +1,12 @@
-if(!me.admin && this.creator !== me.id) {
-    cancel("Unauthorized", 401);
-} else {
+if(me.admin || this.creator !== me.id) {
     var deletedId = this.id;
-    dpd.comments.get(function(comments){
+    dpd.comments.get().then(function(comments){
         comments.forEach(function(comment){
             if(comment.postId == deletedId){
                 dpd.comments.del({id: comment.id});
             }
         });
     });
+} else {
+    cancel("Unauthorized", 401);
 }
